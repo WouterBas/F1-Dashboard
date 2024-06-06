ca55461f6f65b3265d222427a679ae6d5183e7ce
Wouter Bastaens <wouter.bastaens@gmail.com>
2024-06-06 10:24:30 +0200

    add more tests

--- server/tests/circuits.test.ts
+++ server/tests/circuits.test.ts
@@ -7,6 +7,13 @@ describe("Circuits", () => {
     expect(data).not.toBeNull();
     expect(data).toBeInstanceOf(Array);
     expect(data.length).toBeGreaterThan(0);
+    expect(data[0]).toBeObject();
+    expect(data[0]).toHaveProperty("_id");
+    expect(data[0]).toHaveProperty("name");
+    expect(data[0]).toHaveProperty("latestSession");
+    expect(data[0]._id).toBeString();
+    expect(data[0].name).toBeString();
+    expect(data[0].latestSession).toBeNumber();
   });
 
   test("should get circuit by id", async () => {
@@ -18,5 +25,26 @@ describe("Circuits", () => {
     expect(data).toBeInstanceOf(Object);
     expect(data._id).toEqual("664f8e1d76877c9741ba8413");
     expect(data.name).toEqual("Austin");
+    expect(data.duration).toBeNumber();
+    expect(data.selectedDriver).toBeNumber();
+    expect(data.startTime).toBeString();
+    expect(data.sessionInfo.drivers).toBeArray();
+    expect(data.sessionInfo.drivers).toHaveLength(20);
+    expect(data.sessionInfo.sessionKey).toBeNumber();
+  });
+
+  test("should get circuit points", async () => {
+    const response = await fetch(
+      "http://localhost:4000/api/v1/circuit/points/63"
+    );
+    const data: any = await response.json();
+    expect(data).not.toBeNull();
+    expect(data).toBeInstanceOf(Array);
+    expect(data.length).toBeGreaterThan(0);
+    expect(data[0]).toBeObject();
+    expect(data[0]).toHaveProperty("x");
+    expect(data[0]).toHaveProperty("y");
+    expect(data[0].x).toBeNumber();
+    expect(data[0].y).toBeNumber();
   });
 });
--- server/tests/positions.test.ts
+++ server/tests/positions.test.ts
@@ -1,12 +1,32 @@
 import { test, expect, describe } from "bun:test";
 
-import { app } from "../src/index";
+describe("Position", () => {
+  test("Should get positions by key, minute and startdate", async () => {
+    const response = await fetch(
+      "http://localhost:4000/api/v1/position/9515?minute=0&starttime=2024-05-19T13:00:00.000Z"
+    );
+    const data: any = await response.json();
+    expect(response.status).toBe(200);
+    expect(data).not.toBeNull();
+    expect(data.length).toBeGreaterThan(0);
+    expect(data[0]).toBeObject();
+    expect(data[0]).toHaveProperty("timestamp");
+    expect(data[0]).toHaveProperty("entries");
+    expect(data[0].entries).toBeObject();
+  });
 
-// describe("Position", () => {
-//   test("GET /position", async () => {
-//     const res = await app.request("api/v1/position/:key", { method: "GET" });
-//     expect(res.status).toBe(200);
-//     expect(await res.json()).not.toBeNull();
-//     expect(await res.json()).toBeInstanceOf(Array);
-//   });
-// });
+  test("Should get positions by driver, key, startdate and duration", async () => {
+    const response = await fetch(
+      "http://localhost:4000/api/v1/position/1/9472?starttime=2024-03-02T15:01:00.000Z&duration=60000"
+    );
+    const data: any = await response.json();
+    expect(response.status).toBe(200);
+    expect(data).not.toBeNull();
+    expect(data.length).toBeGreaterThan(0);
+    expect(data[0]).toBeObject();
+    expect(data[0]).toHaveProperty("x");
+    expect(data[0]).toHaveProperty("y");
+    expect(data[0].x).toBeNumber();
+    expect(data[0].y).toBeNumber();
+  });
+});
