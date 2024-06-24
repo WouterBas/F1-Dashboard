9a4727e8a664f1ac5da7ee24ab98da10c271b929
Wouter Bastaens <wouter.bastaens@gmail.com>
2024-06-24 11:45:24 +0200

    add github link

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,6 +2,8 @@ import SelectSession from "@/components/home/SelectSession";
 import { apiService } from "@/services/api.service";
 import { SessionList } from "@/types";
 import HomeProvider from "@/components/home/HomeProvider";
+import Link from "next/link";
+import { FaGithub } from "react-icons/fa6";
 
 export default async function Home() {
   async function fetchSessions() {
@@ -16,6 +18,16 @@ export default async function Home() {
 
   return (
     <>
+      <section className="flex justify-end text-xs sm:text-sm">
+        <Link
+          href="https://github.com/WouterBas/F1-Dashboard"
+          target="_blank"
+          className="flex h-fit items-center gap-1 rounded-full border p-1 "
+        >
+          <FaGithub className="text-lg sm:text-xl" />
+          <span className="font-sans font-medium">Github</span>
+        </Link>
+      </section>
       <main className="col-span-2 grid grid-rows-[1fr_auto_1fr] items-center">
         <h2 className="mx-auto  w-11/12 max-w-xs rounded text-center text-lg font-bold sm:text-xl md:max-w-xl md:text-2xl">
           Experience Formula 1 with Detailed Telemetry Insights
